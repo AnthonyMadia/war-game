90ec2561cd45bffabee084d9d9006b63f714f9ee
Anthony Madia <aamadia24@gmail.com>
2022-02-11 12:09:31 -0500

    Add html, basic css, test event listeners

--- js/app.js
+++ js/app.js
@@ -1,18 +1,27 @@
 /*-------------------------------- Constants --------------------------------*/
-const deck = 
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-let playerDeck, playerCard, compDeck, compCard, 
+let playerDeck, playerCard, compDeck, compCard, winner
 
 
 /*------------------------ Cached Element References ------------------------*/
-
-
+const turnCard = document.querySelector('#turn-btn')
+const restartBtn = document.querySelector('#restart-btn')
 
 /*----------------------------- Event Listeners -----------------------------*/
+turnCard.addEventListener('click', showCards)
+
+restartBtn.addEventListener('click', (evt) => {
+  console.log(evt.target)
+})
 
 
 
 /*-------------------------------- Functions --------------------------------*/
+
+
+function showCards(evt) {
+  console.log('turn card!')
+}
 
