a2439b2973151cfb1e9f45868359e05de3c4d880
Anthony Madia <aamadia24@gmail.com>
2022-02-11 14:08:04 -0500

    Add images

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-let playerDeck, playerCard, compDeck, compCard, winner
+let playerDeck, playerCard, compDeck, compCard, warArray
 
 
 /*------------------------ Cached Element References ------------------------*/
@@ -12,16 +12,32 @@ const restartBtn = document.querySelector('#restart-btn')
 /*----------------------------- Event Listeners -----------------------------*/
 turnCard.addEventListener('click', showCards)
 
-restartBtn.addEventListener('click', (evt) => {
-  console.log(evt.target)
-})
+restartBtn.addEventListener('click', render)
 
 
 
 /*-------------------------------- Functions --------------------------------*/
+init()
+
+
+function init() {
+
+
+  render() // render at end of this function
+}
 
 
 function showCards(evt) {
   console.log('turn card!')
 }
 
+function render(evt) {
+  console.log('restart')
+}
+
+// initialize deck with 52 cards
+  // split 
+
+function shuffle() {
+  
+}
